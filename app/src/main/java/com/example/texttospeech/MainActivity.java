3036dc611258144c53ec21b572ae4694a0c1a259
hmkwon511 <hmkwon511@gmail.com>
2023-04-27 14:58:22 +0900

    commit1 : UI 및 strings.xml 완료

---
+++ app/src/main/java/com/example/texttospeech/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.texttospeech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.UtteranceProgressListener;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
